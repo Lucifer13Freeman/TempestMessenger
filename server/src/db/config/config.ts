c2fb02b27b3dad659dfa9c431bdbdc53a9f93cb6
Lucifer13Freeman <Dmitri98LD@gmail.com>
2021-10-03 22:55:55 +0300

    commit

--- server/src/db/config/config.ts
+++ server/src/db/config/config.ts
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT
 }
